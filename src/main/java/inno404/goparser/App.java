417d785ba85a5b4b276f2921bb76ea86ce8aa05b
EdikGoose <edikgoose@gmail.com>
2021-06-01 17:17:50 +0300

    Init lexer/parser

--- src/main/java/inno404/goparser/App.java
+++ src/main/java/inno404/goparser/App.java
@@ -1,14 +1,9 @@
 package inno404.goparser;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
     }
 }
