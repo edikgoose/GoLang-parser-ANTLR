259c819eac33338d6812a5093dbf9a475971c105
EdikGoose <edikgoose@gmail.com>
2021-06-22 14:38:17 +0500

    Final commit

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# GoLang-parser-ANTLR4
+# Go Parser grammar
+This is an ANTLR4 grammar for Go programming language
 
-Inital commit for iskanred:clone
+# Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Version
+* Antlr V4 4.9.2
+* Java 13
+
+
+## How to use
+* Clone this repository to your machine
+* Write Go program into sample.go file
+* Build the project and execute .jar file
+
+
+## Tests
+All test source files are located in ```src/test/resources/tests``` directory. The application was tested on *Windows 10* & *Linux Mint* systems.
+
+# Main contributors
+* Iskander Nafikov, i.nafikov@innopolis.university
+* Eduard Zaripov, e.zaripov@innopolis.university
+
+
--- src/main/java/inno404/goparser/App.java
+++ src/main/java/inno404/goparser/App.java
@@ -5,74 +5,23 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 
 
-public class App
-{
-    private static class GoVisitor<T> extends GoParserBaseVisitor<T>{
-
-        @Override
-        public T visitSourceFile(GoParser.SourceFileContext ctx) {
-            System.out.println("I started");
-            var to_return = super.visitSourceFile(ctx);
-            System.out.println("I finished");
-
-            return to_return;
-        }
-
-        @Override
-        public T visitTypeDecl(GoParser.TypeDeclContext ctx) {
-            var to_return = super.visitTypeDecl(ctx);
-            System.out.println("typeDecl");
-
-            return to_return;
-        }
-
-        @Override
-        public T visitConstDecl(GoParser.ConstDeclContext ctx) {
-            var to_return = super.visitConstDecl(ctx);
-            System.out.println("constDecl");
-
-            return to_return;
-        }
-
-        @Override
-        public T visitVarDecl(GoParser.VarDeclContext ctx) {
-            var to_return = super.visitVarDecl(ctx);
-            System.out.println("varDecl");
-
-            return to_return;
-        }
-    }
-
-
+public class App {
 
     public static void main( String[] args ) throws IOException {
         // Init part
-        CharStream cs = CharStreams.fromStream(System.in);
+        CharStream cs = CharStreams.fromFileName("sample.go");
         GoLexer lexer = new GoLexer(cs);
         CommonTokenStream stream = new CommonTokenStream(lexer);
         GoParser parser = new GoParser(stream);
 
+        parser.sourceFile();
 
-        // Visit Parser Tree
-        var sourceFileContext = parser.sourceFile();
-        GoVisitor<Void> visitor = new GoVisitor<>();
-        visitor.visit(sourceFileContext);
-
-
-        // Output tokens recognized
-        lexer.reset();
-        var tokenList = lexer.getAllTokens();
-        ArrayList<String> tokens = new ArrayList<>(tokenList.size());
-
-        tokenList.forEach(e -> tokens.add(lexer.getVocabulary().getSymbolicName(e.getType())));
-        tokens.removeIf(e -> e.equals("WHITESPACE")); // remove whitespace tokens
-        System.out.println(tokens.size());
-
-        System.out.println(tokens);
+        if (parser.getNumberOfSyntaxErrors() == 0)
+            System.out.println("The input GO program is syntactically correct");
+        else
+            System.out.println("The input GO program has " + parser.getNumberOfSyntaxErrors() + " syntax errors");
     }
 }
